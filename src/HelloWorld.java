458a58ea5d3d87f8424b84c090b0a2c966b99fb7
richardcalata <calatar6@gmail.com>
2018-05-30 08:48:36 -0500

    changed gitignore for java

---
+++ src/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+        int x=10;
+        int y=2;
+        x /= y;
+        y -=x;
+
+//        System.out.println(++x);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+
+//
+//public class Main {
+//    public static void Main(String[] args) {
+//        System.out.println("Hello World!"):
+//    }
+//}
